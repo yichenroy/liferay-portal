99b8b4dc49ab6b162be977c5579ddf59c6ab47e1
Kenji Heigel <kenji.heigel@liferay.com>
2017-07-11 10:18:45 -0700

    LRQA-30892 TEMP Prototype for new Jenkins report timeline

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -518,6 +518,11 @@ public abstract class BaseBuild implements Build {
 	}
 
 	@Override
+	public Element getJenkinsReportElement() {
+		return null;
+	}
+
+	@Override
 	public String getJobName() {
 		return jobName;
 	}
@@ -671,6 +676,8 @@ public abstract class BaseBuild implements Build {
 	@Override
 	public Long getStartTimestamp() {
 		JSONObject buildJSONObject = getBuildJSONObject("timestamp");
+
+		//System.out.println(buildJSONObject.toString());
 
 		if (buildJSONObject == null) {
 			return null;
