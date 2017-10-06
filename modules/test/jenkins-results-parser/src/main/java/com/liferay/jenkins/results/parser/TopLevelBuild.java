3032138c4bce4fd5e60b7a9c67042d6c1604a9ee
yichenroy <yi-chen.tsai@liferay.com>
2017-10-06 12:08:00 -0700

    LRQA-30892 Rename and move get(Timeline|ChartJsScript)Element into TopLevelBuild.java

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuild.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -511,11 +512,36 @@ public class TopLevelBuild extends BaseBuild {
 			bodyElement, h1Element, h2Element,
 			JenkinsReportUtil.getSummaryElement(
 				topLevelBuild, axisBuilds, batchBuilds, testResults),
-			JenkinsReportUtil.getTimelineElement(topLevelBuild, axisBuilds),
+			getJenkinsReportTimelineElement(topLevelBuild, axisBuilds),
 			JenkinsReportUtil.getTopLevelTableElement(topLevelBuild),
 			JenkinsReportUtil.getBatchReportElement(batchBuilds));
 
 		return bodyElement;
+	}
+
+	protected Element getJenkinsReportChartJsScriptElement(
+		String xData, String y1Data, String y2Data) {
+
+		String resource = null;
+
+		try {
+			Class<?> clazz = JenkinsResultsParserUtil.class;
+
+			resource = JenkinsResultsParserUtil.readInputStream(
+				clazz.getResourceAsStream("chart-template.js"));
+		}
+		catch (IOException ioe) {
+		}
+
+		resource = resource.replace("'xData'", xData);
+		resource = resource.replace("'y1Data'", y1Data);
+		resource = resource.replace("'y2Data'", y2Data);
+
+		Element scriptElement = Dom4JUtil.getNewElement("script");
+
+		scriptElement.addText(resource);
+
+		return scriptElement;
 	}
 
 	protected Element getJenkinsReportHeadElement() {
@@ -561,6 +587,70 @@ public class TopLevelBuild extends BaseBuild {
 			headElement, Dom4JUtil.getNewElement("style", null, sb.toString()));
 
 		return headElement;
+	}
+
+	protected Element getJenkinsReportTimelineElement(
+		Build topLevelBuild, Map<String, Build> builds) {
+
+		long topLevelDuration = topLevelBuild.getDuration();
+		long topLevelStartTime = topLevelBuild.getStartTimestamp();
+
+		int dataPoints = 500;
+
+		long[] invocationData = new long[dataPoints];
+		long[] slaveUsageData = new long[dataPoints];
+
+		for (String key : builds.keySet()) {
+			Build build = builds.get(key);
+
+			long buildDuration = build.getDuration();
+			long buildStartTime = build.getStartTimestamp();
+
+			long buildEndTime = buildDuration + buildStartTime;
+
+			long dataEnd =
+				(buildEndTime - topLevelStartTime) * dataPoints /
+					topLevelDuration;
+
+			long dataStart =
+				(buildStartTime - topLevelStartTime) * dataPoints /
+					topLevelDuration;
+
+			for (int i = (int)dataStart; i < dataEnd; i++) {
+				slaveUsageData[i] = ++slaveUsageData[i];
+			}
+
+			invocationData[(int)dataStart] = ++invocationData[(int)dataStart];
+		}
+
+		long[] timeData = new long[dataPoints];
+
+		timeData[0] = 0;
+
+		for (int i = 1; i < timeData.length; i++) {
+			timeData[i] = timeData[0] + i * topLevelDuration / dataPoints;
+		}
+
+		Element canvasElement = Dom4JUtil.getNewElement("canvas");
+
+		canvasElement.addAttribute("height", "300");
+		canvasElement.addAttribute("id", "timeline");
+
+		Element scriptElement = Dom4JUtil.getNewElement("script");
+
+		scriptElement.addAttribute("src", _CHART_JS_FILE);
+		scriptElement.addText("");
+
+		Element chartJSScriptElement = getJenkinsReportChartJsScriptElement(
+			Arrays.toString(timeData), Arrays.toString(slaveUsageData),
+			Arrays.toString(invocationData));
+
+		Element divElement = Dom4JUtil.getNewElement("div");
+
+		Dom4JUtil.addToElement(
+			divElement, canvasElement, scriptElement, chartJSScriptElement);
+
+		return divElement;
 	}
 
 	protected Element getJobSummaryListElement() {
@@ -848,6 +938,9 @@ public class TopLevelBuild extends BaseBuild {
 
 	protected static final Pattern gitRepositoryTempMapNamePattern =
 		Pattern.compile("git\\.(?<repositoryType>.*)\\.properties");
+
+	private static final String _CHART_JS_FILE =
+		"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js";
 
 	private static final long _DOWNSTREAM_BUILDS_LISTING_INTERVAL =
 		1000 * 60 * 5;
