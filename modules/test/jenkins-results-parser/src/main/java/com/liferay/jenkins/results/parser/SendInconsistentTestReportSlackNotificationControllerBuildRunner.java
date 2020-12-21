0f1be1ab8e8c7af834063d1310e48d465270eca9
yichenroy <yi-chen.tsai@liferay.com>
2020-12-21 15:40:57 -0800

    LRCI-1858 Fix regex capturing group syntax

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SendInconsistentTestReportSlackNotificationControllerBuildRunner.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SendInconsistentTestReportSlackNotificationControllerBuildRunner.java
@@ -371,7 +371,7 @@ public class SendInconsistentTestReportSlackNotificationControllerBuildRunner
 	}
 
 	private static final Pattern _testSuiteBranchNameDescriptionPattern =
-		Pattern.compile("(<testSuite>[^\\(]*)\\((<branchName>[^\\)]*)\\)");
+		Pattern.compile("(?<testSuite>[^\\(]*)\\((?<branchName>[^\\)]*)\\)");
 
 	private List<Pair<String, String>> _selectedTestSuiteBranchNamePairs;
 
